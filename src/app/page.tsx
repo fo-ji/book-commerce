bf600792023a1f35a1281b5680934b8fdc726cc0
fo-ji <fumiya1753@gmail.com>
2024-01-10 17:47:35 +0900

    implement book list components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { BookList } from '@/features/books';
+
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <main className="flex flex-wrap justify-center items-center md:mt-32 mt-20">
+      <h2 className="text-center w-full font-bold text-3xl mb-2">
+        Book Commerce
+      </h2>
+      <BookList />
+    </main>
+  );
 }

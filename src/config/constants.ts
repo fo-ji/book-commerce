5dd156b7667eedd4fd9acc62205c52eb37c391a8
fo-ji <fumiya1753@gmail.com>
2024-01-24 11:20:11 +0900

    setting next-auth

---
+++ src/config/constants.ts
@@ -0,0 +1,3 @@
+export const GITHUB_ID = process.env.GITHUB_ID as string;
+export const GITHUB_SECRET = process.env.GITHUB_SECRET as string;
+export const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET as string;
---
+++ src/lib/next-auth/options.ts
@@ -0,0 +1,46 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GitHubProvider from 'next-auth/providers/github';
+
+import { NextAuthOptions } from 'next-auth';
+import { GITHUB_ID, GITHUB_SECRET, NEXTAUTH_SECRET } from '@/config/constants';
+
+import prisma from './prisma';
+
+export const nextAuthOptions: NextAuthOptions = {
+  debug: true,
+  secret: NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      // profile(profile) {
+      //   console.log("Profile Github: ", profile);
+      //   let userRole = "GitHub User";
+      //   if (profile?.email === "shincode0712@gmail.com") {
+      //     userRole = "admin";
+      //   }
+
+      //   return {
+      //     id: profile.id.toString(), // 'id' を string 型に変換
+      //     name: profile.name,
+      //     email: profile.email,
+      //     image: profile.avatar_url,
+      //     role: userRole, // 追加されるカスタムプロパティ
+      //     // 必要に応じて他のプロパティを追加
+      //   };
+      // },
+      clientId: GITHUB_ID,
+      clientSecret: GITHUB_SECRET,
+    }),
+  ],
+  callbacks: {
+    session: ({ session, user }) => {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+        },
+      };
+    },
+  },
+};

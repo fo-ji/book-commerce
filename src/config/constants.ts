60169c004fc73998118bcc5b5d0792090b18502d
fo-ji <fumiya1753@gmail.com>
2024-01-27 14:24:30 +0900

    install microCMS sdk and init setting

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,5 @@
 export const GITHUB_ID = process.env.GITHUB_ID as string;
 export const GITHUB_SECRET = process.env.GITHUB_SECRET as string;
 export const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET as string;
+export const SERVICE_DOMAIN = process.env.NEXT_PUBLIC_SERVICE_DOMAIN as string;
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY as string;
